c1a1ee8b0b9f5435c40ba8d3f74ce2022267f4c1
Christopher Kingsborough <chrisdkingsborough@gmail.com>
2020-10-08 23:54:50 -0400

    Update database.py

--- api/app/routes/database/database.py
+++ api/app/routes/database/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@localhost/listi"
-SQLALCHEMY_DATABASE_URL = "postgres://neefjvqilhgatg:cc9342875dc38280cb275a26f4bcc4546cf1582fae929a0a895b9630c71834f8@ec2-3-214-46-194.compute-1.amazonaws.com:5432/d9k5gv2j90sgs8"
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@localhost/listi"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
